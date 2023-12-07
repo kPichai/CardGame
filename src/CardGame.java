5b9095783719f61427b1b13b77171842b4616c19
kpi <kieran.pichai@menloschool.org>
2023-12-06 19:00:26 -0800

    I completely changed the CardGame class and added a lot of logistical code, the game now runs completely except with 1-3 errors.
    Things to do for next time:
    - make an actual shuffling algorithm
    - fix the removing of cards if another player steals them
    - add the possibility where the player doesnt try to steal someone elses card and just tries to go fish
    - have a full terminal clear after every turn so players hands dont get leaked

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,5 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 // Kieran Pichai CardGame 2023 (CS2)
 public class CardGame {
     final private String[] suits = {"Clubs", "Spades", "Hearts", "Diamonds"};
@@ -47,15 +49,113 @@ public class CardGame {
     }
 
     public void printInstructions() {
-
+        System.out.println("\nWelcome to Go Fish! Below are the rules:");
+        System.out.println("\n- You will play with a friend and alternate turns");
+        System.out.println("- On your turn you are allowed to ask for a card from another player (only a card you already have at least one instance of)");
+        System.out.println("    - If they have that card in their hand they must give you all instances of it, if you guess correctly you also get to keep your turn");
+        System.out.println("    - Otherwise you draw a new card from the deck and pass your turn on");
+        System.out.println("- Everytime you get a set of 4 same cards you place them down and call it a book");
+        System.out.println("    - The game ends once a player gets 7 or more books");
+        System.out.println("\nI hope you have fun playing and good luck!");
+        System.out.println("\n\nNow let the game begin: \n");
     }
 
-    public boolean checkGameEnd() {
-        return (deck.isEmpty()) || (p1.getPoints() > 6) || (p2.getPoints() > 6);
+    public Player checkGameEnd() {
+        checkBooks();
+        if (p1.getPoints() > 6) {
+            return p1;
+        } else if (p2.getPoints() > 6) {
+            return p2;
+        }
+        return null;
     }
+
+    public boolean checkValidTurn(String cardWanted, int turn) {
+        if (turn == 0) {
+            for (int i = 0; i < p1.getHand().size(); i++) {
+                if (p1.getHand().get(i).getRank().equals(cardWanted)) {
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < p2.getHand().size(); i++) {
+                if (p2.getHand().get(i).getRank().equals(cardWanted)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Integer> checkInstances(String nameOfCard, ArrayList<Card> c1) {
+        ArrayList<Integer> removed = new ArrayList<Integer>();
+        for (int i = p2.getHand().size() - 1; i >= 0 ; i--) {
+            if (c1.get(i).getRank().equals(nameOfCard)) {
+                removed.add(i);
+            }
+        }
+        return removed;
+    }
+
     public void runGame() {
+        Scanner sc = new Scanner(System.in);
         printInstructions();
-
+        String response;
+        int turn = 0;
+        Player possibleWinner = null;
+        while (possibleWinner ==  null) {
+            if (turn == 0) {
+                System.out.println(p1.toString());
+                do {
+                    System.out.println("Player 1: What card would you like to fish for? (Your answer can be 2-10 or Jack, Queen, King, Ace and must also be a card currently in your hand)");
+                    response = sc.nextLine();
+                } while (!checkValidTurn(response, turn));
+                ArrayList<Integer> toRemove = checkInstances(response, p2.getHand());
+                if (toRemove.isEmpty()) {
+                    turn = 1;
+                    p1.addCard(deck.deal());
+                    System.out.println("Whoops, you have to Go Fish!");
+                    System.out.println("You drew a " + p1.getHand().get(p1.getHand().size() - 1) + ".");
+                } else {
+                    for (int i = 0; i < toRemove.size(); i++) {
+                        p1.addCard(p2.getHand().get(toRemove.get(i)));
+                    }
+                    for (int i = 0; i < toRemove.size(); i++) {
+                        p2.getHand().remove(toRemove.get(i));
+                    }
+                    System.out.println("Nice job! You gained " + toRemove.size() + " " + response + "'s.");
+                    System.out.println("You get another turn now!");
+                }
+            }
+            System.out.flush();
+            if (turn == 1) {
+                System.out.println(p2.toString());
+                do {
+                    System.out.println("Player 2: What card would you like to fish for? (Your answer can be 2-10 or Jack, Queen, King, Ace and must also be a card currently in your hand)");
+                    response = sc.nextLine();
+                } while (!checkValidTurn(response, turn));
+                ArrayList<Integer> toRemove = checkInstances(response, p1.getHand());
+                if (toRemove.isEmpty()) {
+                    turn = 0;
+                    p2.addCard(deck.deal());
+                    System.out.println("Whoops, you have to Go Fish!");
+                    System.out.println("You drew a " + p2.getHand().get(p2.getHand().size() - 1) + ".");
+                } else {
+                    for (int i = 0; i < toRemove.size(); i++) {
+                        p2.addCard(p1.getHand().get(toRemove.get(i)));
+                    }
+                    for (int i = 0; i < toRemove.size(); i++) {
+                        p1.getHand().remove(toRemove.get(i));
+                    }
+                    System.out.println("Nice job! You gained " + toRemove.size() + " " + response + "'s.");
+                    System.out.println("You get another turn now!");
+                }
+            }
+            System.out.flush();
+            possibleWinner = checkGameEnd();
+        }
+        System.out.println("Congrats on winning " + checkGameEnd().getName() + "!");
+        System.out.println(checkGameEnd().toString());
     }
     public static void main(String[] args) {
         CardGame c1 = new CardGame();
